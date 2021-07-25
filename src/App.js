fb2a74983ae2bd77664e0d78d6e909e05cbd0215
leydev <wesley.waaraujo@gmail.com>
2021-07-25 18:55:48 -0300

    removal webvitals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import React from 'react';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-        </p>
-      </header>
+      <Grid container className={classes.root}>
+        <Grid item xs={4}>
+          <Typography>Filled</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <DeleteIcon />
+          <DeleteForeverIcon />
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
+
 export default App;
+
+{/* 
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit
+    //     </p>
+    //   </header>
+    // </div> */}
