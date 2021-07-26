c1abe63a1a657f63e91ce1bd844558fabda82d50
leydev <wesley.waaraujo@gmail.com>
2021-07-26 17:19:24 -0300

    add fab'

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import { CardUser } from '../components/cards/CardUser';
 import Theme from '../utils/Theme';
 
@@ -10,6 +11,20 @@ function Component() {
       <Box height="100%" overflow="auto" padding="0rem .5rem">
         <Typography variant="h5" style={{ margin: '1rem 0rem' }}>Users</Typography>
         <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <CardUser />
+        <Fab className="fab-button">
+          <AddIcon />
+        </Fab>
       </Box>
     </Theme>
   );
